eb1f7426cf9f1c1c17b4df7651dc078a2f27ba79
Sara Harvey <saraharveynyc@gmail.com>
2021-01-05 14:20:44 -0500

    app maps a hard coded array of books in lieu of API to start, equipped with required key, returns list of title attributes in index.html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import "./index.css";
 
+const books = [
+{id: 1, title: 'Lucky', author: 'Gabrielle Bell', category: 'Comic'},
+{id: 2, title: 'Total Cat Mojo', author: 'Jackson Galaxy', category: 'Non-ficiton'},
+{id: 3, title: 'Boys of Alabama', author: 'Genevieve Hudson', category: 'Fiction'},
+]
+
 export default class App extends React.Component {
 render() {
   return (
-    <div className="App">
-    <h1>Welcome to BookList</h1>
-    </div>
+      <div className="App">
+      {books.map((book) => (
+        <h2 key={book.id}>{book.title}</h2>
+        ))}
+      </div>
     );
   }
 }

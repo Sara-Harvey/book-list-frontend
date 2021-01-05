a03f0641d31dcd4cf6e5df16e96e5206b60c2b77
Sara Harvey <saraharveynyc@gmail.com>
2021-01-05 14:38:46 -0500

    added genre to book card

--- src/BookCard.js
+++ src/BookCard.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class BookCard extends React.Component {
 	render() {
-		return <div><h2>{this.props.book.title}</h2></div>;
+		return <div><h2>{this.props.book.title}</h2><h4>{this.props.book.category}</h4></div>;
 	}
 }
 

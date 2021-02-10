24ea0e3ab865527136d162d407dd1f68958072a4
Sara Harvey <saraharveynyc@gmail.com>
2021-02-10 09:35:09 -0500

    added code to return random story

--- src/containers/RandomContainer.js
+++ src/containers/RandomContainer.js
@@ -1,36 +1,35 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 
-import Stories from '../components/Stories';
+import Stories from '../components/Stories.js';
 import { getData } from '../actions/index.js';
-//import { rollDice } from '../actions/index.js';
 
-export class RandomContainer extends Component {  
+class RandomContainer extends Component {
 
   componentDidMount() {
     this.props.getData();
-    //this.props.store.dispatch(rollDice());
   };
-
-  // dispatch(rollDice())
-  // this.props.store.dispatch(rollDice())
 
   render() {
-    const {stories} = this.props;
+   let randomStory = this.props.stories.sort(() => Math.random() - Math.random()).slice(0, 1);
+   let stories = randomStory
 
     return (
-      <div>
-      <h3>A random story to read or hear</h3>
-        <Stories stories={stories} />
-      </div>
-
-    );
+          <div>
+              <h3>A random story to read or hear</h3>
+              <Stories stories={stories} />            
+          </div>
+        ); 
+      }
   }
-}
 
-  const mapStateToProps = state => {      
-      return { stories: state.stories };
+  const mapStateToProps = (state) => {
+    return { stories: state.stories };
   };
 
-export default connect(mapStateToProps, {getData})(RandomContainer);
+  const mapDispatchToProps = dispatch => {
+    return { getData: () => {dispatch(getData())} };
+  };
+
+export default connect(mapStateToProps, mapDispatchToProps)(RandomContainer);
 

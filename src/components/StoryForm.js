bf0edd91b6aecedc631b7e40a4bd4f26eae0ad77
Sara Harvey <saraharveynyc@gmail.com>
2021-03-09 10:11:00 -0500

    added dropdown menu for category form section

--- src/components/StoryForm.js
+++ src/components/StoryForm.js
@@ -37,7 +37,7 @@ class NewContainer extends React.Component{
         return (
 
         <div className="container text-center">
-          <h4>Add a story of your own</h4>
+          <h3>Add a story of your own</h3>
 
             <Form onSubmit={this.handleSubmit}>
                     
@@ -77,13 +77,19 @@ class NewContainer extends React.Component{
                 placeholder="description" />  
               </Form.Group>
 
-              <Form.Group controlId="formBasicCategory">
+              <h4>Category</h4>
+
+              <Form.Group controlId="formBasicCategory.ControlSelect1">
                 <Form.Control 
-                type="text" 
-                name="category_id" 
-                value={this.state.category_id} 
-                onChange={this.handleChange} 
-                placeholder="category" />  
+                  as="select"
+                  name="category_id"
+                  value={this.state.category_id} 
+                  onChange={this.handleChange} >  
+
+                  <option label="read">1</option>
+                  <option label="listen">2</option>
+
+                </Form.Control>
               </Form.Group>
                     
               <Button variant="primary" type="submit"> Add story </Button>
